5204b8230fefcb87f6ab17ebdc779c7d4b9315ef
Dima Korolchuk <dima.korolchuk@mabbly.com>
2020-07-19 16:52:26 +0300

    Feat(Notifications): Added for auth page

--- src/store/reducers/root-reducer.js
+++ src/store/reducers/root-reducer.js
@@ -1,4 +1,4 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 import quizReducer from './quiz';
 import createReducer from './create';
 import authReducer from './auth';
@@ -6,5 +6,5 @@ import authReducer from './auth';
 export default combineReducers({
   quiz: quizReducer,
   create: createReducer,
-  auth: authReducer
-})
+  auth: authReducer,
+});
--- src/store/store.js
+++ src/store/store.js
@@ -1,1 +1,15 @@
-//TODO: Transfer code there from index.js and export store
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './reducers/root-reducer';
+import thunkMiddleware from 'redux-thunk';
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunkMiddleware));
+const store = createStore(rootReducer, enhancer);
+
+export default store;
